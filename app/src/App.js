5358316daea261ea11af3111c8ca9edc7fa4011a
claudiney63 <claudineyryan75@gmail.com>
2023-12-06 02:46:15 -0300

    Adição de novos components e criação de animação do titulo no css

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,27 @@
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
 
 function App() {
   return (
     <div className="container">
-      
       <h1 className="title">Buscador de CEP</h1>
-      
+
       <div className="containerInput">
-        <input
-          type="text"
-          placeholder="Digite seu cep..."
-        ></input>
+        <input type="text" placeholder="Digite seu cep..."></input>
 
         <button className="buttonSearch">
-
+          <FiSearch size={25} color="#FFFF"></FiSearch>
         </button>
       </div>
 
+      <main>
+        <h2>Cep: 4565432</h2>
+
+        <span>Rua: Teste</span>
+        <span>Complemento: vhhkjl</span>
+        <span>Cila Rosa</span>
+        <span>Campo Grande</span>
+      </main>
     </div>
   );
 }
