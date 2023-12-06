4d29a96b627b3abfb9ff9dcf81053b2e05e80e8f
claudiney63 <claudineyryan75@gmail.com>
2023-12-06 02:28:55 -0300

    Instalados pacote de icons e criando os components

---
+++ app/src/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <div className="container">
+      
+      <h1 className="title">Buscador de CEP</h1>
+      
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+        ></input>
+
+        <button className="buttonSearch">
+
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
